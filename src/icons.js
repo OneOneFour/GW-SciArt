e27c95faebf6af5a8635550eba6146d4806473c2
Robert King <robcking@stanford.edu>
2023-03-13 09:10:27 -0700

    Page v1

--- src/icons.js
+++
@@ -1,8 +0,0 @@
-import { library,icon} from '@fortawesome/fontawesome-svg-core';
-import { faDice,faMound,faMountain } from '@fortawesome/free-solid-svg-icons';
-
-library.add(faDice);
-library.add(faMound);
-library.add(faMountain);
-
-export default icon;
--- src/util.js
+++
@@ -1,5 +0,0 @@
-function bulge(x,xmax){
-    if (x >= xmax) return 0;
-    return (xmax*xmax-x*x)/(xmax*xmax);
-}
-export {bulge};
